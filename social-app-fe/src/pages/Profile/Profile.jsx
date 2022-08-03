6efe222ce1ee970cb9e8583982e726477edfca0c
MinhLV8 <leminh3214789@gmail.com>
2022-08-03 10:02:18 +0700

    Minh commit fe

--- social-app-fe/src/pages/Profile/Profile.jsx
+++ social-app-fe/src/pages/Profile/Profile.jsx
@@ -3,20 +3,19 @@ import PostSide from "../../components/PostSide/PostSide";
 import ProfileCard from "../../components/ProfileCard.jsx/ProfileCard";
 import ProfileLeft from "../../components/ProfileLeft/ProfileLeft";
 import RightSide from "../../components/RightSide/RightSide";
-
 import "./Profile.css";
 
 const Profile = () => {
-    return (
-        <div className="Profile">
-            <ProfileLeft/>
-            <div className="Profile-center">
-                <ProfileCard/>
-                <PostSide/>
-            </div>
-            <RightSide/>
-        </div>
-    );
+  return (
+    <div className="Profile">
+      <ProfileLeft />
+      <div className="Profile-center">
+        <ProfileCard />
+        <PostSide />
+      </div>
+      <RightSide />
+    </div>
+  );
 };
 
 export default Profile;
